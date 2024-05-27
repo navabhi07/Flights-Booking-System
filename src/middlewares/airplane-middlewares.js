45a402e9068d0554ff9fb3b2674457e18cbb11de
kumar Abhinav <krabhinav023@gmail.com>
2024-05-28 00:39:23 +0530

    updated airplane-middlewares validateCreateRequest

--- src/middlewares/airplane-middlewares.js
+++ src/middlewares/airplane-middlewares.js
@@ -1,47 +1,19 @@
-// middleware/validateCreateAirplane.js
 const { StatusCodes } = require('http-status-codes');
-const { Airplane } = require('../models'); // Import your Airplane model
-const {AppError} = require('../utils/errors/app-error');
 
-const validateCreateRequest = async (req, res, next) => {
-    try {
-        const { modelNumber } = req.body;
+const { ErrorResponse } = require('../utils/common');
+const AppError = require('../utils/errors/app-error');
 
-        //Check if the modelNumber already exists in the database
-        const existingAirplane = await Airplane.findOne({ where: { modelNumber:modelNumber } });
-        if (existingAirplane) {
-            return res
+function validateCreateRequest(req, res, next) {
+    if(!req.body.modelNumber) {
+        ErrorResponse.message = 'Something went wrong while creating airplane';
+        ErrorResponse.error = new AppError(['Model Number not found in the oncoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res
                 .status(StatusCodes.BAD_REQUEST)
-                .json(
-                     new AppError('the modelNumber already exists in the database',StatusCodes.BAD_REQUEST)
-                )
-            }
-        
-
-        // If the modelNumber is unique, proceed with other validations
-        const { capacity } = req.body;
-        if (!modelNumber || !capacity || typeof modelNumber !== 'string' || /^\d+$/.test(modelNumber)||isNaN(capacity) || parseInt(capacity) < 1) {
-            return res
-            .status(StatusCodes.BAD_REQUEST)
-            .json(new AppError('Invalid input, please check again',StatusCodes.BAD_REQUEST))
-        }
-
-        next();
-    } catch (error) {
-        // Handle any unexpected errors
-        console.error('Error in validation middleware:', error);
-        res.status(500).json({
-            message: 'Internal Server Error',
-            success: false
-        });
+                .json(ErrorResponse);
     }
-};
-
-
-
-module.exports =
-{
-    validateCreateRequest,
+    next();
 }
 
-
+module.exports = {
+    validateCreateRequest
+}
