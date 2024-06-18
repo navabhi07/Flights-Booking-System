26c8562d41c64982b80ce793db9ce5456e142787
kumar Abhinav <krabhinav023@gmail.com>
2024-06-18 11:12:20 +0530

    Added flights model with foreign key constraint defined during model

---
+++ src/migrations/20240617065502-create-flight.js
@@ -0,0 +1,82 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Flights', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      flightNumber: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      airplaneId: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references:{
+          model:'Airplanes',
+          key:'id'
+        }
+      },
+      departureAirportId: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        references:{
+          model:'Airports',
+          key:'code'
+        }
+      },
+      arrivalAirportId: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        references:{
+          model:'Airports',
+          key:'code'
+        }
+      },
+      arrivalTime: {
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      departureTime: {
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      boardingGate: {
+        type: Sequelize.STRING
+      },
+      totalSeats: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Flights');
+  }
+};
+
+
+/*
+command to create this model and light models in models
+```
+npx sequelize model:generate --name Flight --attributes flightNumber:string,airplaneId:integer,
+departureAirportId:integer,arrivalAirportId:integer,arrivalTime:date,departureTime:date,price:integer,
+boardingGate:string,totalSeats:integer
+
+*/
