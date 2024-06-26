52f57e2d49aebfebc9d4ca75375d1245e4bb32e8
kumar Abhinav <krabhinav023@gmail.com>
2024-06-26 12:16:14 +0530

    added airport resource crud

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,6 +1,8 @@
+
 const CityRepository = require('./city-repository');
 
 module.exports = {
     AirplaneRepository:require('./airplane-repository'),
-    CityRepository:require('./city-repository')
+    CityRepository: require('./city-repository'),
+    AirportRepository: require('./airports-repository')
 }  
