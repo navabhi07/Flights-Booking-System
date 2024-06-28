0fb8774bd469da23611cadc4456ed1115077f1f9
kumar Abhinav <krabhinav023@gmail.com>
2024-06-29 01:39:06 +0530

    FEAT: Added filters for get all flights

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -8,6 +8,15 @@ class FlightRepository extends CrudRepository{
    constructor(){
     super(flight);
    }
+
+   //custom repo
+   async getAllFlights(filter, sort) {
+      const response = await flight.findAll({
+          where: filter,
+          order: sort
+      });
+      return response;
+  }
 }
 
 module.exports=FlightRepository;
